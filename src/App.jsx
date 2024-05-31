f213f61ce3370422e63ac8c7abb3871e283da350
Jerome <jerome.bourscheid@gmail.com>
2024-05-31 16:55:16 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+import './assets/styles/index.scss';
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
